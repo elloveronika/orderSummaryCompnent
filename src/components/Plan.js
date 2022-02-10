77d3c5e37460cc89c172dff883e49f987ca16852
elloveronika <ello.veronika92@gmail.com>
2022-02-09 23:10:45 -0800

    still working on css, plan section

--- src/components/Plan.js
+++ src/components/Plan.js
@@ -3,9 +3,9 @@ import React from "react";
 const Plan = () => {
   return (
     <div className="plan--div">
-      <img></img>
-      <div className="plan--anual">
-        <h1>Anual Plan</h1>
+      <img src="images/icon-music.svg" />
+      <div className="plan--annual">
+        <h1>Annual Plan</h1>
         <span>$59.99/year</span>
       </div>
       <span>Change</span>
--- src/components/Summary.js
+++ src/components/Summary.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Summary = () => {
   return (
-    <div>
-      <h1>Order Summary</h1>
-      <p>
+    <div className="sum--div">
+      <h1 className="sum--h1">Order Summary</h1>
+      <p className="sum--p">
         You can now listen to millions of songs audiobooks, and podcasts on any
         device anywhere you like
       </p>
